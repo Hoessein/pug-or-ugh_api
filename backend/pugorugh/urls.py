a810b80607ce2e335b4dab08aebed0774898b821
Hoessein Abdullahi <habdullahi@saa.nl>
2019-04-10 17:04:26 +0200

    Get next undecided dog

--- backend/pugorugh/urls.py
+++ backend/pugorugh/urls.py
@@ -12,20 +12,16 @@ urlpatterns = format_suffix_patterns([
     url(r'^api/user/login/$', obtain_auth_token, name='login-user'),
     url(r'^api/user/$', views.UserRegisterView.as_view(), name='register-user'),
     # url(r'^api/dog/(?P<pk>\d+)/liked/next/$', views.ListUndecidedDogs.as_view(), name='liked-dogs'),
-    url(r'^api/user/preferences/$', views.UserPreferences.as_view(), name='user-detail'),
-    url(r'api/dog/(?P<pk>-\d+)/undecided/next/$', views.ShowMeSomethingPLeaseView.as_view(), name='undecided-dogs'),
+    url(r'api/user/preferences/$', views.UserPreferences.as_view(), name='user-detail'),
+    url(r'api/dog/(?P<pk>-\d+)/undecided/next/$', views.NextDogView.as_view(), name='undecided-dogs'),
+    url(r'api/dog/(?P<pk>\d+)/undecided/next/$', views.NextDogView.as_view(), name='undecided-dogs'),
+
     url(r'^favicon\.ico$',
         RedirectView.as_view(
             url='/static/icons/favicon.ico',
             permanent=True
         )),
 
-    #
-    # url(r'^$'/api/undecided, views.ListDog.as_view(), name='dog_list'),
-    #
-    # url(r'^api/dog/(?P<pk>\d+)/liked/next/$',
-    #     views.RetrieveUpdateDestroyDog.as_view(),
-    #     name='dog_detail'),
 
     url(r'^$', TemplateView.as_view(template_name='index.html'))
 ])
--- backend/pugorugh/views.py
+++ backend/pugorugh/views.py
@@ -4,6 +4,8 @@ from rest_framework import permissions
 from rest_framework import generics
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import Http404
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
 
 
 from rest_framework.generics import CreateAPIView, RetrieveUpdateAPIView
@@ -22,14 +24,25 @@ class UserPreferences(generics.RetrieveUpdateAPIView):
     queryset = models.UserPref.objects.all()
     serializer_class = serializers.UserPrefSerializer
 
-class ShowMeSomethingPLeaseView(generics.RetrieveUpdateAPIView):
-    # queryset = models.Dog.objects.all()
+
+class NextDogView(generics.RetrieveUpdateAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
+    queryset = models.Dog.objects.all()
     serializer_class = serializers.DogSerializer
 
     def get_object(self):
-        queryset = models.Dog.objects.all().get(pk=1)
-        print("queryset = ",  queryset)
-        return queryset
+        # queryset = models.Dog.objects.get(pk=1)
+        # # print(self.queryset)
+        # pk = int(self.kwargs.get('pk'))
+        # pk += 1
+        # print(pk)
+        # return self.get_queryset().get_next_by_id()
 
-#kwargs.get is de pk die binnenkomt
+
+        p = self.kwargs.get('pk')
+
+        m = self.get_queryset().filter(pk__gt=p).order_by('pk').first()
+        return m
+
+        #kwargs.get is de pk die binnenkomt
 #get_object gets a single item
