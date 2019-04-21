abdb37d529734ee8155bd8523c981cfce2e500b1
Hoessein Abdullahi <habdullahi@saa.nl>
2019-04-21 14:34:39 +0200

    added comments and docstrings for test.py

--- backend/pugorugh/tests.py
+++ backend/pugorugh/tests.py
@@ -14,10 +14,10 @@ from . import serializers
 from . import views
 
 
-class ViewTest(TestCase):
+class ViewAndModelTest(TestCase):
 
     def setUp(self):
-        """Creates a test object for every model before every test is ran"""
+        """Creates a test object for every model before the tests are ran"""
         self.test_user = User.objects.create_user(
             username='test_that_dog',
             email='test@dog.nl',
@@ -34,10 +34,11 @@ class ViewTest(TestCase):
         )
 
         self.test_user_pref = UserPref.objects.create(
-            user=self.test_user,
             age='a',
             gender='f',
-            size='l'
+            size='l',
+            user=self.test_user,
+
         )
 
         self.test_user_dog = UserDog.objects.create(
@@ -46,18 +47,26 @@ class ViewTest(TestCase):
             status='u'
         )
 
-    def test_register_view(self):
+    def test_user_register(self):
+        """Tests if a user can be registered """
         factory = APIRequestFactory()
-        request = factory.post(reverse('register-user'), {'username': 'Jump', 'password': 'liverpool',})
+        request = factory.post(reverse('register-user'),
+                               {'username': 'Jump', 'password': 'liverpool'})
         view = views.UserRegisterView.as_view()
         response = view(request)
 
+        # one objects gets created in the setup method
+        # the other gets created in this method by sending a post request
         self.assertEqual(User.objects.count(), 2)
         self.assertEqual(response.status_code, 201)
 
     def test_already_registerd_user(self):
+        """Tests if you can register two users with the same username"""
         factory = APIRequestFactory()
-        request = factory.post(reverse('register-user'), {'username': 'test_that_dog', 'password': 'liverpool', })
+        # Post the same username as creaed in the setup method
+        request = factory.post(reverse('register-user'),
+                               {'username': 'test_that_dog', 'password': 'liverpool'})
+
         view = views.UserRegisterView.as_view()
         response = view(request)
 
@@ -65,20 +74,23 @@ class ViewTest(TestCase):
         self.assertEqual(response.status_code, 400)
 
     def test_user_pref(self):
+        """Tests that after login in you can edit your user preferences"""
         factory = APIRequestFactory()
         request = factory.get(reverse('user-pref'))
+
         force_authenticate(request, user=self.test_user)
         view = views.UserPreferencesView.as_view()
         response = view(request)
-
         serializer = serializers.UserPrefSerializer(self.test_user_pref)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # Compares the serialized data to the response data
         self.assertEqual(response.data, serializer.data)
 
     def test_dog_creation(self):
+        """Tests if dogs can be created"""
         factory = APIRequestFactory()
-
+        # React sends in a -1
         request = factory.get(reverse('undecided-dogs', kwargs={'pk': '-1'}))
 
         force_authenticate(request, user=self.test_user)
@@ -87,10 +99,13 @@ class ViewTest(TestCase):
         response = view(request, pk='-1')
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # There should be a dog created in the setup method
         self.assertEqual(Dog.objects.count(), 1)
-        self.assertEqual(Dog.objects.get(pk=1).name, 'Francesca')
+        # Francesca has been created in the setup method
+        self.assertEqual(Dog.objects.all().get(pk=1).name, 'Francesca')
 
     def test_liked_dog_creation(self):
+        """Tests if only liked dogs are being displayed"""
         factory = APIRequestFactory()
         request = factory.put(reverse('like-dog', kwargs={'pk': '1'}))
 
@@ -102,11 +117,15 @@ class ViewTest(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(Dog.objects.count(), 1)
         self.assertEqual(UserDog.objects.filter(status='l').count(), 1)
+        self.assertEqual(UserDog.objects.filter(status='u').count(), 0)
+        self.assertEqual(UserDog.objects.filter(status='d').count(), 0)
 
     def test_liked_dog_list(self):
+        """Tests if a status of a dog can be changed to liked"""
         factory = APIRequestFactory()
         request = factory.get(reverse('liked-dogs', kwargs={'pk': '-1'}))
 
+        # Changing the status of the created dog in the setup method to liked
         disliked_dog = UserDog.objects.filter(dog_id=self.test_dog.pk, user=self.test_user).get()
         disliked_dog.status = 'l'
         disliked_dog.save()
@@ -118,9 +137,11 @@ class ViewTest(TestCase):
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(Dog.objects.count(), 1)
+        # checks if the status has been changed
         self.assertEqual(UserDog.objects.filter(status='l').count(), 1)
 
     def test_disliked_dog_creation(self):
+        """Tests if only disliked dogs are being displayed"""
         factory = APIRequestFactory()
         request = factory.put(reverse('dislike-dog', kwargs={'pk': '1'}))
 
@@ -134,6 +155,7 @@ class ViewTest(TestCase):
         self.assertEqual(UserDog.objects.filter(status='d').count(), 1)
 
     def test_disliked_dog_list(self):
+        """Tests if a status of a dog can be changed to dislikee"""
         factory = APIRequestFactory()
         request = factory.get(reverse('disliked-dogs', kwargs={'pk': '-1'}))
 
@@ -151,6 +173,7 @@ class ViewTest(TestCase):
         self.assertEqual(UserDog.objects.filter(status='d').count(), 1)
 
     def test_undecided_dog_creation(self):
+        """Tests if only undecided dogs are being displayed"""
         factory = APIRequestFactory()
         request = factory.put(reverse('undecided-dog', kwargs={'pk': '1'}))
 
@@ -164,8 +187,10 @@ class ViewTest(TestCase):
         self.assertEqual(UserDog.objects.filter(status='u').count(), 1)
 
     def test_undecided_dog_list(self):
+        """Tests if a status of a dog can be changed to undecided"""
         factory = APIRequestFactory()
         request = factory.get(reverse('undecided-dogs', kwargs={'pk': '-1'}))
+
         force_authenticate(request, user=self.test_user)
 
         view = views.ListUndecidedDogsView.as_view()
@@ -174,4 +199,5 @@ class ViewTest(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(Dog.objects.count(), 1)
         self.assertEqual(UserDog.objects.filter(status='u').count(), 1)
-        self.assertEqual(Dog.objects.get(pk=1).name, 'Francesca')
+        # Francesca should be by default have the undecided status
+        self.assertEqual(Dog.objects.all().get(pk=1).name, 'Francesca')
